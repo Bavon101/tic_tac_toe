e71cc94a4efb8d0758b1b4d7644a3bd4ba27ec61
Bavon101 <bavonfate@gmail.com>
2022-03-26 17:55:09 +0300

    Create a tile widget to dislay moves

--- lib/src/widgets/board_tile.dart
+++ lib/src/widgets/board_tile.dart
@@ -1,12 +1,35 @@
 import 'package:flutter/material.dart';
 
 class BoardTile extends StatelessWidget {
-  const BoardTile({ Key? key }) : super(key: key);
+  const BoardTile(
+      {Key? key, this.active = true, required this.value, required this.onTap})
+      : super(key: key);
+
+  /// [BoardTile] acts as the card to display the current tapped value
+  ///
+  /// Keeping a record of the UI
+
+  final String value;
+  final Function onTap;
+  final bool active;
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      
-    );
+    return InkWell(
+      onTap: active? () => onTap():null,
+      child: Container(
+           decoration: BoxDecoration(
+              color: Colors.white,
+              border: Border.all(color: Theme.of(context).primaryColor)),
+          child: Center(
+            child: Text(
+              value.toUpperCase(),
+              style: TextStyle(
+                  color: value == 'o' ? Colors.green : Colors.red,
+                  fontWeight: FontWeight.bold,
+                  fontSize: 80),
+            ),
+          ),
+      ));
   }
 }
