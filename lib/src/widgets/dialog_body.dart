7db85b5634bf4e0b5807a3580f47bb5aaf566e33
Bavon101 <bavonfate@gmail.com>
2022-03-26 17:45:47 +0300

    create project structure

---
+++ lib/src/widgets/dialog_body.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class DialogBody extends StatelessWidget {
+  const DialogBody({ Key? key }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
